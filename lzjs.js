bb54b529824279f5751a72d365386d4695436e0b
polygonplanet <polygon.planet.aqua@gmail.com>
2014-11-27 03:32:49 +0900

    1.2.3

--- lzjs.js
+++ lzjs.js
@@ -3,7 +3,7 @@
  *
  * @description  Compression by LZ algorithm in JavaScript.
  * @fileOverview Data compression library
- * @version      1.2.0
+ * @version      1.2.3
  * @date         2014-11-27
  * @link         https://github.com/polygonplanet/lzjs
  * @copyright    Copyright (c) 2014 polygon planet <polygon.planet.aqua@gmail.com>
